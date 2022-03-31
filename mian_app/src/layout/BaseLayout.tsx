415e7e59c480e4f56e449096883084bd6aca0527
lniev <1479441581@qq.com>
2022-03-31 23:41:56 +0800

    layout

--- mian_app/src/layout/BaseLayout.tsx
+++ mian_app/src/layout/BaseLayout.tsx
@@ -1,59 +1,64 @@
-import React, {useState} from 'react';
-import {Avatar, Descriptions} from 'antd';
-import {UserOutlined} from '@ant-design/icons';
-import ProLayout, {PageContainer} from '@ant-design/pro-layout';
+import React, { useState } from 'react';
+import { Avatar, Descriptions } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import ProLayout, { PageContainer } from '@ant-design/pro-layout';
 import 'antd/dist/antd.css';
 import '@ant-design/pro-layout/dist/layout.css';
-import {level1Menu, level2Menu} from './menu';
-import {useLocation, useNavigate,Link} from 'react-router-dom';
-import Router from '../router/router'
-
+import { level1Menu, level2Menu } from './menu';
+import { useLocation, useNavigate, Link } from 'react-router-dom';
+import Router from '../router/router';
 
 export default (props) => {
-    // const location = useLocation();
-    const [pathname, setPathname] = useState('/home');
-    return (
-        <ProLayout
-            location={{
-                pathname: pathname
-                // location.pathname,
-            }}
-            style={{height: '100vh'}}
-            collapsedButtonRender={false}
-            collapsed
-            // iconfontUrl="//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js"
-            route={{
-                routes: level1Menu,
-            }}
-            headerRender={false}
-            menuItemRender={(item, dom) => <Link to={item.path} onClick={() => {
-                setPathname(item.path)
-            }
-            }>{dom}</Link>}
-            disableContentMargin
+  // const location = useLocation();
+  const [pathname, setPathname] = useState('/home');
+  return (
+    <ProLayout
+      location={{
+        pathname: pathname,
+        // location.pathname,
+      }}
+      style={{ height: '100vh' }}
+      collapsedButtonRender={false}
+      collapsed
+      // iconfontUrl="//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js"
+      route={{
+        routes: level1Menu,
+      }}
+      headerRender={false}
+      menuItemRender={(item, dom) => (
+        <Link
+          to={item.path}
+          onClick={() => {
+            setPathname(item.path);
+          }}
         >
-            {/*<ProLayout*/}
-            {/*  location={{*/}
-            {/*    pathname: location.pathname,*/}
-            {/*  }}*/}
-            {/*  route={{*/}
-            {/*    routes: level2Menu,*/}
-            {/*  }}*/}
-            {/*  navTheme="light"*/}
-            {/*  style={{*/}
-            {/*    height: '400px',*/}
-            {/*  }}*/}
-            {/*  rightContentRender={() => (*/}
-            {/*    <div>*/}
-            {/*      <Avatar shape="square" size="small" icon={<UserOutlined />} />*/}
-            {/*    </div>*/}
-            {/*  )}*/}
-            {/*  menuHeaderRender={false}*/}
-            {/*>*/}
-            {/*<PageContainer>*/}
-                <Router/>
-            {/*</PageContainer>*/}
-            {/*</ProLayout>*/}
-        </ProLayout>
-    );
+          {dom}
+        </Link>
+      )}
+      disableContentMargin
+    >
+      {/*<ProLayout*/}
+      {/*  location={{*/}
+      {/*    pathname: location.pathname,*/}
+      {/*  }}*/}
+      {/*  route={{*/}
+      {/*    routes: level2Menu,*/}
+      {/*  }}*/}
+      {/*  navTheme="light"*/}
+      {/*  style={{*/}
+      {/*    height: '400px',*/}
+      {/*  }}*/}
+      {/*  rightContentRender={() => (*/}
+      {/*    <div>*/}
+      {/*      <Avatar shape="square" size="small" icon={<UserOutlined />} />*/}
+      {/*    </div>*/}
+      {/*  )}*/}
+      {/*  menuHeaderRender={false}*/}
+      {/*>*/}
+      <PageContainer>
+        <Router />
+      </PageContainer>
+      {/*</ProLayout>*/}
+    </ProLayout>
+  );
 };
--- mian_app/src/router/router.tsx
+++ mian_app/src/router/router.tsx
@@ -1,43 +1,35 @@
 // router.js
-import {Routes, Route, Link, Outlet} from 'react-router-dom';
-import {lazy, Suspense} from 'react';
-import Home from '../page/home/home.page'
-import BaseLayout from "../layout/BaseLayout";
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import Home from '../page/home/home.page';
+import BaseLayout from '../layout/BaseLayout';
 
 const ReactApp = lazy(() => import('../page/reactApp/reactApp'));
 // const Home = lazy(() => import('../page/home/home.page'));
 export default function AppRoute() {
-    const loading = <div>Loading...</div>;
-    return (
-        <div>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        // <Suspense fallback={loading}>
-                        //   <Home />
-                        // </Suspense>
-                        <BaseLayout><Outlet/></BaseLayout>
-                    }
-                >
-                    <Route
-                        path="/home"
-                        element={
-                            <Home/>
-                        }
-                    />
-                    <Route
-                        path="/rp"
-                        element={
-                            <Suspense fallback={loading}>
-                                <ReactApp/>
-                            </Suspense>
-                        }
-                    />
-                </Route>
-
-
-            </Routes>
-        </div>
-    );
+  const loading = <div>Loading...</div>;
+  return (
+    <div>
+      <Routes>
+        {/* <Route
+          path="/"
+          element={
+            // <Suspense fallback={loading}>
+            //   <Home />
+            // </Suspense>
+            <div>/////</div>
+          }
+        ></Route> */}
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/rp"
+          element={
+            <Suspense fallback={loading}>
+              <ReactApp />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
