644ad915b6f461dda93590506e86c0c5dc0f9642
lniev <1479441581@qq.com>
2022-04-01 14:07:23 +0800

    calendar 组件

--- child_apps/react-app/src/App.tsx
+++ child_apps/react-app/src/App.tsx
@@ -1,12 +1,12 @@
-import { Routes, Route, Outlet, useLocation } from "react-router-dom";
 import Router from "./router/router";
 import "./App.css";
+
 function App() {
-	return (
-		<div className="App">
-			<Router />
-		</div>
-	);
+    return (
+        <div className="App">
+            <Router/>
+        </div>
+    );
 }
 
 export default App;
--- child_apps/react-app/src/pages/Calendar/Calendar.tsx
+++ child_apps/react-app/src/pages/Calendar/Calendar.tsx
@@ -1,12 +1,33 @@
-import { useState } from "react";
+import {useState} from "react";
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import {sliceEvents, createPlugin} from '@fullcalendar/react';
 
 const CalendarPage = () => {
-	const [state, setState] = useState();
-	return (
-		<div>
-			<header>calendar</header>
-		</div>
-	);
+    const [state, setState] = useState();
+
+    function renderEventContent(eventInfo) {
+        return (
+            <>
+                <b>{eventInfo.timeText}</b>
+                <i>{eventInfo.event.title}</i>
+            </>
+        )
+    }
+
+    return (
+        <div>
+            <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+                events={[
+                    {title: 'event 1', date: '2019-04-01'},
+                    {title: 'event 2', date: '2019-04-02'}
+                ]}
+                eventContent={renderEventContent}
+            />
+        </div>
+    );
 };
 
 export default CalendarPage;
