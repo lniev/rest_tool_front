b9f9081c7be368cb68843ee541d0c2e293b7e6e3
lniev <1479441581@qq.com>
2022-04-11 00:23:16 +0800

    线上部署配置

--- mian_app/src/page/reactApp/reactApp.tsx
+++ mian_app/src/page/reactApp/reactApp.tsx
@@ -1,13 +1,14 @@
 import { memo, useState, useEffect, FC } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import microApp from '@micro-zoe/micro-app';
-
+import Config from '../../../../config';
 interface IProps {}
 
 const ReactAppPage: FC<IProps> = (props) => {
   const navigate = useNavigate();
   const location = useLocation();
-  const baseRoute = '/rp/';
+  const isProd = process.env.NODE_ENV === 'production';
+  const baseRoute = Config.child_react_baseRoute;
 
   function pushState(path) {
     navigate(path);
@@ -16,10 +17,11 @@ const ReactAppPage: FC<IProps> = (props) => {
   useEffect(() => {
     microApp.setData('react-app', { pathname: location.pathname.replace(baseRoute, '/'), pushState });
   });
+  const url = Config.child_react_url
 
   return (
     <div>
-      <micro-app name="react-app" url="http://localhost:8077/" baseroute={baseRoute} keep-alive></micro-app>
+      <micro-app name="react-app" url={url} baseroute={baseRoute} keep-alive></micro-app>
     </div>
   );
 };
