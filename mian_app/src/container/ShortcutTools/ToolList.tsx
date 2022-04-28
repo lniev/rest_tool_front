a44bd2bec16bad00d463322fbd91127afb80ee4b
lniev <1479441581@qq.com>
2022-04-28 15:25:15 +0800

    [功能添加] 图片转base64

--- mian_app/src/container/ShortcutTools/ToolList.tsx
+++ mian_app/src/container/ShortcutTools/ToolList.tsx
@@ -1,22 +1,43 @@
-import json from "../../assets/img/tools/json.png";
-import JsonFormat from "../JsonFormat/JsonFormat.container";
+import json from '../../assets/img/tools/json.png';
+import JsonFormat from '../JsonFormat/JsonFormat.container';
+import Base64 from '../Base64/Base64.container';
+import base64 from '../../assets/img/tools/base64.png'
 
-const ToolList = [{
-  title: 'JSON Format',
-  type: 'jsonFormat',
-  label: 'JSON格式化',
-  resizeProps:{
-    defaultSize: {
-      x: 600,
-      height: 900,
-    }
+const ToolList = [
+  {
+    title: 'JSON Format',
+    type: 'jsonFormat',
+    label: 'JSON格式化',
+    resizeProps: {
+      defaultSize: {
+        width: 600,
+        height: 900,
+      },
+    },
+    icon: () => {
+      return <img src={json} />;
+    },
+    content: () => {
+      return <JsonFormat />;
+    },
   },
-  icon: () => {
-    return <img src={json}/>
+  {
+    title: '图片Base64编码',
+    type: 'base64编码',
+    label: '图片转base64',
+    resizeProps: {
+      defaultSize: {
+        width: 600,
+        height: 740,
+      },
+    },
+    icon: () => {
+      return <img src={base64} />;
+    },
+    content: () => {
+      return <Base64 />;
+    },
   },
-  content: () => {
-    return (<JsonFormat/>)
-  }
-}]
+];
 
 export default ToolList
